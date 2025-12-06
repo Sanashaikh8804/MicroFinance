1cb3e902efc18973ea3493bff455e42a30b5e18c
Sanashaikh8804 <sanashaikh8804@gmail.com>
2025-12-06 14:33:59 +0530

    routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express= require('express');
+const router= express.Router();
+
+router.post("/aadhar", aadhar);
+router.post("/pan", pan);
+router.post("business_description"  , business_description);
+router.post("/previous_micro_loans", previous_micro_loans);
+router.post("/gst_number", gst_number);
+
+module.exports= router;
