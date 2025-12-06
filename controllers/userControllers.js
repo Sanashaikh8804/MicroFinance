13441d810cdf1a62983a355d4d287b00e7f811d0
Sanashaikh8804 <sanashaikh8804@gmail.com>
2025-12-06 15:15:53 +0530

    Controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,49 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+//create new name
+const createUser=  asyncHandler(async (req, res) => {
+    console.log{"req body is", req.body};
+    const { name, phone_number, password, email, aadhar, pan, business_description, previous_micro_loans, gst_number } = req.body;
+    
+    if (!name || !phone_number || !password || !email || !aadhar || !pan || !business_description || !previous_micro_loans || !gst_number) {
+        res.status(400);
+        throw new Error("Please fill all the fields");
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+    const user = await User.create({
+        name,
+        phone_number,
+        password,
+        email,
+        aadhar,
+        pan,
+        business_description,
+        previous_micro_loans,
+        gst_number,
+    });
+    if (user) { 
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            phone_number: user.phone_number,
+            email: user.email,
+            aadhar: user.aadhar,
+            pan: user.pan,
+            business_description: user.business_description,
+            previous_micro_loans: user.previous_micro_loans,
+            gst_number: user.gst_number,
+        });
+    }
+    else {
+        res.status(400);
+        throw new Error("User not found");
+    }
+});
+
+module.exports= { createUser };
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,6 @@
 const express= require('express');
 const router= express.Router();
 
-router.post("/name", Name);
-router.post("/phone_number", phone_number);
-router.post("/password", password)
-router.post("/email", email);
-router.post("/aadhar", aadhar);
-router.post("/pan", pan);
-router.post("business_description"  , business_description);
-router.post("/previous_micro_loans", previous_micro_loans);
-router.post("/gst_number", gst_number);
+router.post("/createUser", createUser);
 
 module.exports= router;
